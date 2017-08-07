12ebfd899ba67e02dac34bc7c4eddafba03ec47c
Omar Shekfeh <o.shekfeh@gmail.com>
2017-08-08 01:42:05 +0300

    Success to run the system but without using the data provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+//
+// import { NavController } from 'ionic-angular';
+// import { Data } from '../../providers/data';
+//
+// @Component({
+//   selector: 'page-home',
+//   templateUrl: 'home.html'
+// })
+// export class HomePage {
+//   posts: any = []
+//   constructor(public navCtrl: NavController, public _data: Data) {
+//     this.posts._data.Posts.subscribe((post) => {this.posts.push(post)})
+//   }
+//
+// }
 
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Data } from '../../providers/data';
+
+const data = [{
+  title: 'post1',
+  body: 'hi',
+  id: 0
+}]
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  posts: any = []
-  constructor(public navCtrl: NavController, public _data: Data) {
-    this.posts._data.Posts.subscribe((post) => {this.posts.push(post)})
+  posts: any
+  constructor(public navCtrl: NavController) {
+    this.posts = data;
   }
 
 }
